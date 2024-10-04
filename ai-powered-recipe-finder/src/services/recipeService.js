2173f15deaf8e5f84845ccbd88afa7538fa98c54
Bodo Bogdan-Mihai <80922283+waterAddicted@users.noreply.github.com>
2024-10-04 21:27:57 +0300

    Update recipeService.js

--- ai-powered-recipe-finder/src/services/recipeService.js
+++ ai-powered-recipe-finder/src/services/recipeService.js
@@ -1,5 +1,5 @@
 export const fetchRecipes = async (query) => {
-    // Înlocuiește acest endpoint cu cel real pe care îl vei folosi
+    
     const response = await fetch(`https://api-ai-recipe.com/search?query=${query}`);
     const data = await response.json();
     return data.recipes;
